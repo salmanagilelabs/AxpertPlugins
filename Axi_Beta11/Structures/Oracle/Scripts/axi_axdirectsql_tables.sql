8211dc3da146d8ed8580f077c7aa77cf719b8e81
salmanagilelabs <salman.s@agile-labs.com>
2026-06-25 12:44:45 +0530

    fix:

--- Axi_Beta11/Structures/Oracle/Scripts/axi_axdirectsql_tables.sql
+++ Axi_Beta11/Structures/Oracle/Scripts/axi_axdirectsql_tables.sql
@@ -275,7 +275,7 @@ CREATE TABLE axdirectsql_metadata (
     axdirectsql_metadatarow NUMBER(10) NULL,
     fldname VARCHAR2(100) NULL,
     fldcaption VARCHAR2(100) NULL,
-    "normalized" VARCHAR2(3) NULL,
+    normalized VARCHAR2(3) NULL,
     sourcetable VARCHAR2(50) NULL,
     sourcefld VARCHAR2(50) NULL,
     CONSTRAINT aglaxdirectsql_metadataid PRIMARY KEY (axdirectsql_metadataid)
@@ -520,30 +520,6 @@ Insert into AXDIRECTSQL (AXDIRECTSQLID,CANCEL,SOURCEID,MAPNAME,USERNAME,MODIFIED
 
 <<
 Insert into AXDIRECTSQL (AXDIRECTSQLID,CANCEL,SOURCEID,MAPNAME,USERNAME,MODIFIEDON,CREATEDBY,CREATEDON,WKID,APP_LEVEL,APP_DESC,APP_SLEVEL,CANCELREMARKS,WFROLES,SQLNAME,DDLDATATYPE,SQLSRC,SQLSRCCND,SQLTEXT,PARAMCAL,SQLPARAMS,ACCESSSTRING,GROUPNAME,SQLQUERYCOLS,CACHEDATA,CACHEINTERVAL,ENCRYPTEDFLDS,ADSDESC,SMARTLISTCND,PAGINATION,APPLYDIMENSIONS) values (99999999990037,'F',0,null,'rekhancia',to_date('20-05-26','DD-MM-RR'),'rekhancia',to_date('20-05-26','DD-MM-RR'),null,1,1,null,null,null,'axi_adscolumnlist',null,'Metadata',1,'select b.fldcaption || ''('' || b.fldname || '')'' displaydata,
-       b.fldname name,
-       b.fldcaption caption,
-       b.normalized,
-       b.fdatatype,
-       f.srctf,
-       f.srcfld,
-       CASE
-           WHEN lower(sqltext) LIKE ''%--axp_filter%''
-           THEN ''T''
-           ELSE ''F''
-       END AS filters
-from axdirectsql a
-left join axpdef_smartlist c
-       on a.sqlname = c.adsname
-left join axpdef_smartlist_mdata b
-       on b.axpdef_smartlistid = c.axpdef_smartlistid
-left join axpflds f
-       on b.srctransid = f.tstruct
-      and b.srcfldname = f.fname
-where a.sqlname = :param1','param1','param1~Character~','ALL',null,null,'F','6 Hr',null,null,null,null,null)
->>
-
-
-Insert into AXDIRECTSQL (AXDIRECTSQLID,CANCEL,SOURCEID,MAPNAME,USERNAME,MODIFIEDON,CREATEDBY,CREATEDON,WKID,APP_LEVEL,APP_DESC,APP_SLEVEL,CANCELREMARKS,WFROLES,SQLNAME,DDLDATATYPE,SQLSRC,SQLSRCCND,SQLTEXT,PARAMCAL,SQLPARAMS,ACCESSSTRING,GROUPNAME,SQLQUERYCOLS,CACHEDATA,CACHEINTERVAL,ENCRYPTEDFLDS,ADSDESC,SMARTLISTCND,PAGINATION,APPLYDIMENSIONS) values (99999999990037,'F',0,null,'rekhancia',to_date('20-05-26','DD-MM-RR'),'rekhancia',to_date('20-05-26','DD-MM-RR'),null,1,1,null,null,null,'axi_adscolumnlist',null,'Metadata',1,'select b.fldcaption || ''('' || b.fldname || '')'' displaydata,
            b.fldname name,
            b.fldcaption caption,
            b.normalized,
@@ -558,7 +534,7 @@ Insert into AXDIRECTSQL (AXDIRECTSQLID,CANCEL,SOURCEID,MAPNAME,USERNAME,MODIFIED
     left join axdirectsql_metadata b on a.axdirectsqlid = b.axdirectsqlid
     where a.sqlname = :param1',
     'param1','param1~Character~','ALL',null,null,'F','6 Hr',null,null,null,null,null)
-
+>>
 
 
 
